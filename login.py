d17af23c34301d8c2c191b1876244438c7c8545f
C3P9 <111432851+C3P9@users.noreply.github.com>
2022-08-17 13:58:29 +0200

    Création du fichier login.py

---
+++ login.py
@@ -0,0 +1,12 @@
+# Page de login 
+
+username = user_username
+password = user_password
+
+expected_username = user_username
+expected_password = user_password
+
+if (username == expected_username) && (password == expected_password) :
+  print("Welcome")
+else :
+  print("Access denied")
